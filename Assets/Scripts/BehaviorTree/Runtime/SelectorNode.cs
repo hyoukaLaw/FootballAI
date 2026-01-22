c45c9e6f017e4141bf8b9676b967aed313535885
hyouka <hyouka_law@outlook.com>
2026-01-22 18:59:57 +0800

    SelectorNode 的Abort Self

--- Assets/Scripts/BehaviorTree/Runtime/SelectorNode.cs
+++ Assets/Scripts/BehaviorTree/Runtime/SelectorNode.cs
@@ -17,6 +17,23 @@ namespace BehaviorTree.Runtime
         {
             LastTickFrame = Time.frameCount;
             OnStart();
+            for (int i = 0; i < _currentIndex; i++) // Gemini:前面哪个条件节点突然行了，要执行那个节点（还未验证）
+            {
+                // 偷偷运行排在前面的高优先级节点
+                var status = ChildrenNodes[i].Execute();
+
+                // 关键区别：Sequence 是怕前面由 Success 变 Failure
+                // Selector 是看前面有没有由 Failure 变 Success
+                if (status == NodeState.SUCCESS)
+                {
+                    // 【中止触发】！
+                    // 1. 既然前面的节点成功了，现在正在运行的这个低级任务（比如远程射击）就没用了，杀掉它。
+                    ChildrenNodes[_currentIndex].OnEnd();
+                    // 2. 指针指回那个成功的节点（以便下一帧直接从它开始，或者这一帧直接算它赢）
+                    _currentIndex = i+1;
+                    break;
+                }
+            }
             
             for (int i = _currentIndex; i < ChildrenNodes.Count; i++)
             {
