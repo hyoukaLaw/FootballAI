232d8a36c19906d03999653add2f66fd3ade356b
hyouka <hyouka_law@outlook.com>
2026-02-03 18:59:31 +0800

    去掉抢球的随机性

--- Assets/Scripts/BehaviorTree/Runtime/Nodes/TaskTackle.cs
+++ Assets/Scripts/BehaviorTree/Runtime/Nodes/TaskTackle.cs
@@ -8,28 +8,17 @@ namespace BehaviorTree.Runtime
 
         public override NodeState Evaluate()
         {
-            // 检查抢断保护期：如果在保护期内，不允许任何抢断 // 为什么可以自己抢断自己？？？
-            // 检查必要条件
-
             GameObject owner = Blackboard.Owner;
             GameObject ballHolder = Blackboard.MatchContext.GetBallHolder();
-
-            // 检查是否在抢断范围内
-            float distanceToBallHolder = Vector3.Distance(owner.transform.position, ballHolder.transform.position);
-
-            // 尝试抢断
             float tackleChance = CalculateTackleChance(owner, ballHolder);
-            float random = Random.Range(0, 1f);// Random.Range(0.8f, 0.85f); // (0,1)
             
-            if(random <= tackleChance)
+            if(tackleChance >= 0.5f)
             {
-                // 抢断成功！
                 StealBall(owner);
                 return NodeState.SUCCESS;
             }
             else
             {
-                // 抢断失败
                 BlackboardUtils.StartStun(Blackboard, 0.5f);
                 return NodeState.FAILURE;
             }
